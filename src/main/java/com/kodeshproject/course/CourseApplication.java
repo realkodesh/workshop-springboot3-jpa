8e6bd1ce2ea229b4796a7991644bd37212ac8b74
Guilherme Lira <liraguilherme186@gmail.com>
2023-08-06 12:20:49 -0300

    Project created

---
+++ src/main/java/com/kodeshproject/course/CourseApplication.java
@@ -0,0 +1,17 @@
+package com.kodeshproject.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CourseApplication.class, args);
+	
+		
+	
+	}
+
+}
