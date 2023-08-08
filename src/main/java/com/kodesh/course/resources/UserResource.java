a39937ce7ce399a6be3b3faa10df13e459bbbfe3
Guilherme Lira <liraguilherme186@gmail.com>
2023-08-08 10:27:06 -0300

    H2 database, test profile, JPA

--- src/main/java/com/kodesh/course/resources/UserResource.java
+++ src/main/java/com/kodesh/course/resources/UserResource.java
@@ -1,4 +1,4 @@
-package com.kodeshproject.course.resources;
+package com.kodesh.course.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.kodeshproject.course.entities.User;
 
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
-		
-	}
 
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
 }
