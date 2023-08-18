9ab826b31e505cc54af3b7926528bf392279270d
Guilherme Lira <liraguilherme186@gmail.com>
2023-08-18 17:10:37 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/kodeshproject/course/entities/Category.java
+++ src/main/java/com/kodeshproject/course/entities/Category.java
@@ -5,32 +5,32 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
-public class Category  implements Serializable{
-	
-	
+public class Category implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
-	
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
-	@Transient
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
-	
-	public Category(){
-		
+
+	public Category() {
+
 	}
 
 	public Category(Long id, String name) {
@@ -54,12 +54,10 @@ public class Category  implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 
 	public Set<Product> getProducts() {
 		return products;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -77,8 +75,5 @@ public class Category  implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
-
-	
 
 }
--- src/main/java/com/kodeshproject/course/entities/Product.java
+++ src/main/java/com/kodeshproject/course/entities/Product.java
@@ -9,16 +9,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-
 
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,7 +26,11 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb-product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
